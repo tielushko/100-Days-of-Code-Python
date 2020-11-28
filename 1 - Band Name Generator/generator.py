61a1e597facc1e7ca606847af4b129ebc5b71703
Oleg Tielushko <tielushko@mail.usf.edu>
2020-11-27 23:27:19 -0500

    DAY 1 YAY

---
+++ 1 - Band Name Generator/generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the band name generator")
+city_name = input("What is the city you grew up in?\n")
+pet_name = input("What is the name of your pet?\n")
+
+print(f'Your brand name could be: {city_name} {pet_name}')
